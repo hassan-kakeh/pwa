8db5251f7ab911916933e3bcbfb032ea4c7c74c7
Your Name <hassan.kakeh@eshtrigroup.com>
2022-12-29 13:54:39 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa-test';
+  todos!: any;
+  readonly VAPID_PUBLIC_KEY =
+    'BK6LlOn86fkAtLt9CZsSEVLlwOk93nD7PxPLI2WMcRn1-dKHrMXtGxk9K1Mqn55DPCVVSBhlKU_GgiA9_Bg1MiM';
+  constructor(private swPush: SwPush, private http: HttpClient) {}
+  ngOnInit(): void {
+    if ('contacts' in navigator) {
+      console.log('Supported');
+    }
+  }
+  onSubscription() {
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY,
+      })
+      .then((sub) => {
+        console.log('Notification Subscription', sub);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  loadTodos() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .subscribe((data) => {
+        console.log(data);
+        this.todos = data;
+      });
+  }
+  comments: any;
+
+  loadComments() {
+    this.comments = this.http.get(
+      'https://jsonplaceholder.typicode.com/comments'
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    MatButtonModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
